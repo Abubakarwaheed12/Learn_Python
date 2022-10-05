d1d8784dd2d17b4442e83d4d29efff9465538c0a
abu <abubakarwaheed6346528@gmail.com>
2022-10-05 14:36:22 +0500

    learning fast

--- chapter 3/sum.py
+++ chapter 3/sum.py
@@ -4,3 +4,30 @@ while num<=10:
     total +=num
     num+=1
 print(total)
+
+#  same program in for loop
+nam=1
+eq=0
+
+for name in range(10):
+    eq+=nam
+    nam+=1
+
+print(eq)
+
+
+# sum of numbers in string
+# user_input=input("enter more than one numbers")
+# tot=0
+# for i in range(0 , len(user_input)):
+#     tot+=int(user_input[i])
+# print(tot)
+
+#  ex: 4 ask username and print the index and character 
+name=input("enter your name")
+tem=""
+for i in range(0, len(name)):
+    if name[i] not in tem:
+        print(f"{name[i]} : {name.count(name[i])}")
+        tem+=name[i]
+
