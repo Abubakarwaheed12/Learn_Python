6a42fe30a3fc85b106f844478102beeac2ef1343
abu <abubakarwaheed6346528@gmail.com>
2022-10-12 23:39:49 +0500

    ...

--- chapter 17 INHERITENCE/inheritence.py
+++ chapter 17 INHERITENCE/inheritence.py
@@ -1,1 +1,16 @@
 # INHERITENCE 
+
+
+class phone:
+    def __init__(self , brand , model_name , price):
+        self.brand=brand
+        self.model_name=model_name
+        self._price=max(price , 0)
+
+    def full_name(self):
+        return f"full nae is {self.brand}   {self.model_name}"
+        
+    def make_a_call(self, phone):
+        return f"calling {phone} ......"
+
+    
